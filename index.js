1ea3d943fecb0905d2e0c10daadc692b79837875
aaishaholaleye <morayonuurah@gmail.com>
2023-08-13 14:27:42 +0100

    Updated index.js to make it functional

--- index.js
+++ index.js
@@ -7,26 +7,48 @@
 let basicnum = document.getElementById("numtoconvert")
 let convertbtn = document.getElementById("convert-btn")
 
-let lengthcalc = document.getElementById("lengthcalc")
-let volumecalc = document.getElementById("volumecalc")
-let masscalc = document.getElementById("masscalc")
+let lengthcalcs = document.getElementById("lengthcalc")
+let volumecalcs = document.getElementById("volumecalc")
+let masscalcs = document.getElementById("masscalc")
 
+
+function lengthcalc(){
+
+    let metertofeet = (basicnum * 3.281).toFixed(3)
+    let feettometer = (basicnum / 3.281).toFixed(3)
+    lengthcalcs.innerHTML = `${basicnum} meters = ${metertofeet} feet | ${basicnum} feet = ${feettometer} meters `
+}
+
+function volumecalc(){
+
+    let litertogallon = (basicnum * 0.264).toFixed(3)
+    let gallontoliter = (basicnum / 0.264).toFixed(3)
+    volumecalcs.innerHTML = `${basicnum} liters = ${litertogallon} gallons | ${basicnum} gallons = ${gallontoliter} liters `
+
+}
+
+function masscalc(){
+    
+    let kilostopounds = (basicnum * 2.204).toFixed(3)
+    let poundstokilos = (basicnum / 2.204).toFixed(3)
+    masscalcs.innerHTML = `${basicnum} kilos = ${kilostopounds} pounds | ${basicnum} pounds = ${poundstokilos} kilos `
+
+}
 
 convertbtn.addEventListener("click", function(){
-    basicnum = parseInt(basicnum.value)
+    basicnum = parseInt(basicnum.value);
 
-    let metertofeet = basicnum * 3.281
-    let feettometer = basicnum / 3.281
-    lengthcalc.innerHTML = `${basicnum} meters = ${metertofeet.toFixed(3)} feet | ${basicnum} feet = ${feettometer.toFixed(3)} meters `
+    lengthcalc();
     
-    let litertogallon = basicnum * 0.264
-    let gallontoliter = basicnum / 0.264
-    volumecalc.innerHTML = `${basicnum} liters = ${litertogallon.toFixed(3)} gallons | ${basicnum} gallons = ${gallontoliter.toFixed(3)} liters `
+    volumecalc();
+
+    masscalc()
     
-    let kilostopounds = basicnum * 2.204
-    let poundstokilos = basicnum / 2.204
-    masscalc.innerHTML = `${basicnum} kilos = ${kilostopounds.toFixed(3)} pounds | ${basicnum} pounds = ${poundstokilos.toFixed(3)} kilos `
+    
     
 
 })
+
+
+
 
